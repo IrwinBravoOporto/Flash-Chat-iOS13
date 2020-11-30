345233229dfb2988e1a910fc4f32b0401a90ebb2
irwin <irwinbravooporto@gmail.com>
2020-11-30 10:33:06 -0500

    finish proyect

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var btnRegister: UIButton!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        animationTilleApp()
+        btnLogin.layer.cornerRadius = 20
+        btnRegister.layer.cornerRadius = 20
     }
-    
+}
 
+
+//MARK: - AnimationTitle
+extension WelcomeViewController{
+    func animationTilleApp(){
+        let titleText = K.appName
+        var charIndex = 0.0
+        for letter in titleText{
+            print("_")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+
+platform :ios, '13.0'
+use_frameworks!
+
+install! 'cocoapods', :deterministic_uuids => false, :warn_for_multiple_pod_sources => false, :warn_for_unused_master_specs_repo => false
+
+
+target 'Flash Chat iOS13' do
+  
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'IQKeyboardManagerSwift'
+  
+  
+end
